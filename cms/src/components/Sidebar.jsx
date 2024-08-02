dcf1983989e512ce80f381d2eefac29ef4e61814
Arya Wibowo Jati <94813894+jateez@users.noreply.github.com>
2024-08-02 21:47:52 +0700

    fix: bug on fan

--- cms/src/components/Sidebar.jsx
+++ cms/src/components/Sidebar.jsx
@@ -23,15 +23,6 @@ export default function Sidebar() {
       <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 " aria-label="Sidebar">
         <div className="flex flex-col h-full px-3 pb-4 overflow-y-auto bg-white">
           <ul className="flex flex-col items-between space-y-2 font-medium">
-            {/* <li> */}
-            {/* <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                <svg className="w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                </svg>
-                <span className="ms-3">Dashboard</span>
-              </a> */}
-            {/* </li> */}
             <li>
               <Link to={"/"} className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
                 <svg
@@ -133,8 +124,6 @@ export default function Sidebar() {
                 </svg>
 
                 <span className="flex-1 ms-3 whitespace-nowrap">Categories</span>
-                {/* <spanx
-        class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full">Pro</spanx> */}
               </Link>
             </li>
             <li>
@@ -148,15 +137,17 @@ export default function Sidebar() {
                 </svg>
 
                 <span className="flex-1 ms-3 whitespace-nowrap">Add Staff</span>
-                <spanx class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full">Admin Only</spanx>
+                <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full">Admin Only</span>
               </Link>
             </li>
           </ul>
-          <div className="h-full content-end pb-4">
-            <div className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group font-medium">
-              <span className="flex-1 ms-3 whitespace-nowrap">Privileges: Staff|Admin</span>
+          <Link to={}>
+            <div className="h-full content-end pb-4">
+              <div className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group font-medium">
+                <span className="flex-1 ms-3 whitespace-nowrap">{"Logout"}</span>
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       </aside>
     </>
